d197f4368f23d805442c7e8b0a6fdb3060687c59
Sanjeev <snjvnxyz@gmail.com>
2024-09-14 19:39:36 +0530

    completed PWM config. v1

--- main.c
+++ main.c
@@ -16,9 +16,17 @@ int main(void)
     GPIO_PORTF_DEN_R = 0x01F;            /* set PORTF pins 4 : 0 pins */
     GPIO_PORTF_DIR_R = 0x0E;            /*  */
     GPIO_PORTF_PUR_R = 0x11;            /* PORTF0 and PORTF4 are pulled up */
-    GPIO_PORTF_AFSEL_R = 0x00;
+    GPIO_PORTF_AFSEL_R = 0x08; // configuring the green LED for alternate function - PWM
+    GPIO_PORTF_PCTL_R =0x00005000;
 
-
+    PWM1_CTL_R = 0x00;
+    PWM1_3_CTL_R = 0x00; // enable after PWM is fully configured
+    PWM1_3_GENB_R = 0x32; // when counter hits zero, drive pwm_out = 0; when counter hits cmpA, drive pwm_out = 1;
+    PWM1_3_LOAD_R = 160;
+    PWM1_3_CMPA_R = 79; // 79 for 50% duty cycle
+    PWM1_3_CMPA_R = 0;
+    PWM1_3_CTL_R = 0x01; // enable
+    PWM1_ENABLE_R = 0x80;
     return 0;
 }
 
