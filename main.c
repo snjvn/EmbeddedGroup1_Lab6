8237d3829c9f948f691f90b864c175bb70fc1f2c
Sanjeev <snjvnxyz@gmail.com>
2024-09-14 18:55:45 +0530

    configuring ports and pwm module...

--- main.c
+++ main.c
@@ -6,6 +6,21 @@
  * main.c
  */
 
-void main(){
 
+int main(void)
+{
+    SYSCTL_RCGC2_R |= 0x00000020;       /* enable clock to GPIOF */
+    SYSCTL_RCGC0_R |= 0x00100000;       /* enable clock to PWM */
+    GPIO_PORTF_LOCK_R = 0x4C4F434B;     /* unlock commit register */
+    GPIO_PORTF_CR_R = 0x1F;             /* make PORTF configurable */
+    GPIO_PORTF_DEN_R = 0x01F;            /* set PORTF pins 4 : 0 pins */
+    GPIO_PORTF_DIR_R = 0x0E;            /*  */
+    GPIO_PORTF_PUR_R = 0x11;            /* PORTF0 and PORTF4 are pulled up */
+    GPIO_PORTF_AFSEL_R = 0x00;
+
+
+    return 0;
 }
+
+
+
