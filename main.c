24f8ba14ee22b48a52872e7a6a0f4d3009a1a11f
Sanjeev <snjvnxyz@gmail.com>
2024-09-15 22:52:49 +0530

    variable duty cycle

--- main.c
+++ main.c
@@ -6,7 +6,9 @@
  * main.c
  */
 
-
+void GPIOInterrupt(void);
+uint32_t PORTF_Interrupt = 0x00;
+int duty = 80;
 int main(void)
 {
     SYSCTL_RCGC2_R |= 0x00000020;       /* enable clock to GPIOF */
@@ -17,20 +19,45 @@ int main(void)
     GPIO_PORTF_DEN_R = 0x01F;            /* set PORTF pins 4 : 0 pins */
     GPIO_PORTF_DIR_R = 0x0E;            /*  */
     GPIO_PORTF_PUR_R = 0x11;            /* PORTF0 and PORTF4 are pulled up */
-
     GPIO_PORTF_AFSEL_R = 0x08;
     GPIO_PORTF_PCTL_R = 0x00007000;
+
+    NVIC_EN0_R = 0x40000000; // 30th bit controls PORTF
+    GPIO_PORTF_IS_R = 0x00; // interrupt sensitivity - edge
+    GPIO_PORTF_IEV_R = 0x00;
+    GPIO_PORTF_IM_R = 0x11; // unmasking both switches
 
     TIMER1_CTL_R = 0x00;
     TIMER1_CFG_R = 0x04;
     TIMER1_TBMR_R = 0x0A;
     TIMER1_TBILR_R= 160;
-    TIMER1_TBMATCHR_R = 100;
+    TIMER1_TBMATCHR_R = duty;
     TIMER1_CTL_R = 0x0100;
-    while(1){;}
+
+    while(1){
+        NVIC_EN0_R = 0x40000000; // 30th bit controls PORTF
+        GPIO_PORTF_IM_R = 0x11; // unmasking both switches
+        TIMER1_TBMATCHR_R = duty;
+    }
 
     return 0;
 }
 
 
+void GPIOInterrupt(){
 
+    PORTF_Interrupt = GPIO_PORTF_RIS_R;
+    NVIC_EN0_R = 0x00000000; // 30th bit controls PORTF
+    GPIO_PORTF_IM_R = 0x00; // masking both switches
+
+    if (PORTF_Interrupt == 0x01){
+        GPIO_PORTF_CR_R = 0x01;
+        duty += 8;
+    }
+
+    else if (PORTF_Interrupt == 0x10){
+            GPIO_PORTF_CR_R = 0x10;
+            duty -= 8;
+        }
+
+}
