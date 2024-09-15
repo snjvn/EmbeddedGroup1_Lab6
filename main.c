332c5812058f307346e83b1c6b92a9225f827ad7
Sanjeev <snjvnxyz@gmail.com>
2024-09-15 22:13:11 +0530

    GPTM1 with CCP at PORTF3

--- main.c
+++ main.c
@@ -11,6 +11,8 @@ int main(void)
 {
     SYSCTL_RCGC2_R |= 0x00000020;       /* enable clock to GPIOF */
     SYSCTL_RCGCPWM_R |= 0x02; // enable clock to PWM module 1
+    SYSCTL_RCGCTIMER_R = 0x02;
+    SYSCTL_RCGCGPIO_R = 0x20;
 //    SYSCTL_RCGC0_R |= 0x00100000;       /* enable clock to PWM */
     GPIO_PORTF_LOCK_R = 0x4C4F434B;     /* unlock commit register */
     GPIO_PORTF_CR_R = 0x1F;             /* make PORTF configurable */
@@ -19,18 +21,16 @@ int main(void)
     GPIO_PORTF_PUR_R = 0x11;            /* PORTF0 and PORTF4 are pulled up */
 
     GPIO_PORTF_AFSEL_R = 0x08;
-    GPIO_PORTF_PCTL_R = 0x00005000;
+    GPIO_PORTF_PCTL_R = 0x00007000;
     GPIO_PORTF_DATA_R = 0x04;
 
-    PWM1_3_CTL_R = 0x00;
-    PWM1_3_LOAD_R = 160;
-    PWM1_3_GENB_R = 0x32;
-    PWM1_3_CMPA_R = 79; // 79 for 50% duty cycle
-    PWM1_3_CMPB_R = 0x00;
-    PWM1_3_CTL_R = 0x01;
-    PWM1_ENABLE_R = 0x80;
-
-    GPIO_PORTF_DATA_R = 0x02;
+    TIMER1_CTL_R = 0x00;
+    TIMER1_CFG_R = 0x04;
+    TIMER1_TBMR_R = 0x0A;
+    TIMER1_CTL_R = 0x4000;
+    TIMER1_TBILR_R= 160;
+    TIMER1_TBMATCHR_R = 80;
+    TIMER1_CTL_R |= 0x0100;
     while(1){;}
 
     return 0;
